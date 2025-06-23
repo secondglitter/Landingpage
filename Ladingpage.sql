6b09344b31a9541daf2ff7b22f6b4a2b09c1f5a8
Joshua Gonzalez <69594954+secondglitter@users.noreply.github.com>
2025-06-23 01:49:51 -0500

    Se sube el script de la base de datos

---
+++ Ladingpage.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE landingpage;
+
+use landingpage;
+
+CREATE TABLE contactos (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(100),
+  telefono VARCHAR(20),
+  correo VARCHAR(100),
+  mensaje TEXT,
+  fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+select * from contactos;
