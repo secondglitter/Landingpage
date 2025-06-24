a1e36716997d89f77a8e5e82bd997fa3ed05c6a4
Miguel Ortega <miguelangelortegamazun@gmail.com>
2025-06-23 20:51:48 -0500

    check de terminos y condiciones y agregacion del Recaptcha

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,13 @@
-const mysql = require('mysql2/promise');
+  const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',         // ej. 'root'
-  password: '',  // la que usas en Workbench
-  database: 'landingpage',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+  const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',         // ej. 'root'
+    password: '',  // la que usas en Workbench
+    database: 'landingpage',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  });
 
-module.exports = pool;
+  module.exports = pool;
--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,36 @@ const express = require('express');
 const cors = require('cors');
 const pool = require('./db');
 
+// Solución para fetch en CommonJS
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const RECAPTCHA_SECRET_KEY = '6LdWaWsrAAAAAOYBLL9HgF2xqcol78i2bnKUFcKi';
+
 app.post('/api/contacto', async (req, res) => {
   try {
-    const { nombre, telefono, correo, mensaje } = req.body;
-    console.log("Datos recibidos:", req.body);
+    const { nombre, telefono, correo, mensaje, captcha } = req.body;
+
+    if (!captcha) {
+      return res.status(400).json({ error: 'Captcha no proporcionado' });
+    }
+
+    const verificationURL = `https://www.google.com/recaptcha/api/siteverify?secret=${RECAPTCHA_SECRET_KEY}&response=${captcha}`;
+
+    const captchaResponse = await fetch(verificationURL, { method: 'POST' });
+    const captchaData = await captchaResponse.json();
+
+    if (!captchaData.success) {
+      return res.status(400).json({ error: 'Falló la validación del captcha' });
+    }
 
     const sql = `
       INSERT INTO contactos (nombre, telefono, correo, mensaje)
       VALUES (?, ?, ?, ?)
     `;
-
     await pool.query(sql, [nombre, telefono, correo, mensaje]);
 
     res.status(200).json({ mensaje: 'Formulario guardado con éxito' });
